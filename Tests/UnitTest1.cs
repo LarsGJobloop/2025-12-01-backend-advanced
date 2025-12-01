247bd313250ecc47aa2436622bb7fc5fff90841d
zabronax <zabronax@gmail.com>
2025-12-01 11:12:56 +0100

    feat: define example test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -5,6 +5,15 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Arrange
+        var a = 2;
+        var b = 2;
+        var expected = 5;
 
+        // Act
+        var result = a + b;
+
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
