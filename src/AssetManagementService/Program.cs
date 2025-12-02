3bca6d869a67e0411fc209514378801f6a0b590a
zabronax <zabronax@gmail.com>
2025-12-02 10:46:13 +0100

    fix: http testing skeleton

--- src/AssetManagementService/Program.cs
+++ src/AssetManagementService/Program.cs
@@ -4,3 +4,6 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// Needed for the MVC test framework
+public partial class Program { }
---
+++ tests/AssetManagementService.Tests/TestRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+
+namespace AssetManagementService.Tests;
+
+public class TestRequst : IClassFixture<WebApplicationFactory<Program>>
+{
+  private readonly WebApplicationFactory<Program> _factory;
+
+  public TestRequst(WebApplicationFactory<Program> factory)
+  {
+    _factory = factory;
+  }
+
+  [Fact]
+  public void HelloTest()
+  {
+    // Create New Asset registration object
+
+    // Make HTTP request against the API
+
+    // Assert that it does what we expect
+  }
+}
