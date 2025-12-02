4714482f1a75cd321fa841e77749517a9a444961
zabronax <zabronax@gmail.com>
2025-12-02 11:00:19 +0100

    red: add health check spec

---
+++ tests/AssetManagementService.Tests/HealthCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace AssetManagementService.Tests;
+
+public class HealthCheck : IClassFixture<WebApplicationFactory<Program>>
+{
+  private readonly WebApplicationFactory<Program> _factory;
+
+  public HealthCheck(WebApplicationFactory<Program> factory)
+  {
+    _factory = factory;
+  }
+
+  [Fact]
+  public async Task WhenQuerrying_HealthEndpoint_ServerRespondsWithOK()
+  {
+    // Given a fresh client
+    var client = _factory.CreateClient();
+
+    // When querry the Health Endpoint
+    var response = await client.GetAsync("/health");
+
+    // The response is a sucess
+    response.EnsureSuccessStatusCode();
+  }
+}
