1fc80efdd5049128f61f0ecfce12dd34feaeb505
zabronax <zabronax@gmail.com>
2025-12-04 21:00:54 +0100

    green: update invalid asset reservation implementation

--- src/ReservationService/Program.cs
+++ src/ReservationService/Program.cs
@@ -1,19 +1,39 @@
 using Contracts.ReservationService;
+using Contracts.AssetManagement;
+using ReservationService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Register AssetManagementServiceClient
+// Hardcoding for now as we have no test that says otherwise.
+var assetManagementServiceUrl = "http://localhost:5002";
+builder.Services.AddHttpClient<AssetManagementServiceClient>(client =>
+{
+  client.BaseAddress = new Uri(assetManagementServiceUrl);
+});
+
 var app = builder.Build();
 
 var reservations = new Dictionary<string, ReservationResponse>();
 
 app.MapGet("/health", () => "OK");
 
-app.MapPost("/reservations", (ReservationRequest request) =>
+app.MapPost("/reservations", async (ReservationRequest request, AssetManagementServiceClient assetClient) =>
 {
+  // Validate that the asset exists in the Asset Management Service
+  var assetResponse = await assetClient.Client.GetAsync($"/assets/{request.AssetId}");
+  if (assetResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+  {
+    return Results.BadRequest();
+  }
+
+  // Validate that the asset is not already reserved
   if (reservations.ContainsKey(request.AssetId))
   {
     return Results.BadRequest();
   }
 
+  // Create a new reservation
   var newReservation = new ReservationResponse
   {
     Id = Guid.NewGuid(),
@@ -22,8 +42,10 @@ app.MapPost("/reservations", (ReservationRequest request) =>
     EndDate = request.EndDate
   };
 
+  // Persist the reservation
   reservations[request.AssetId] = newReservation;
 
+  // Return the reservation
   return Results.Ok(newReservation);
 });
 
