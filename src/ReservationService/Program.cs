74971811530a54c3d6951584eace804654fa9ef8
zabronax <zabronax@gmail.com>
2025-12-04 16:03:03 +0100

    green: implement valid reservation request specification

--- src/ReservationService/Program.cs
+++ src/ReservationService/Program.cs
@@ -3,6 +3,32 @@ var app = builder.Build();
 
 app.MapGet("/health", () => "OK");
 
+app.MapPost("/reservations", (ReservationRequest request) =>
+{
+  return Results.Ok(new ReservationResponse
+  {
+    Id = Guid.NewGuid(),
+    AssetId = request.AssetId,
+    StartDate = request.StartDate,
+    EndDate = request.EndDate
+  });
+});
+
 app.Run();
+
+class ReservationRequest
+{
+  public required string AssetId { get; init; }
+  public required DateTime StartDate { get; init; }
+  public required DateTime EndDate { get; init; }
+}
+
+class ReservationResponse
+{
+  public required Guid Id { get; init; }
+  public required string AssetId { get; init; }
+  public required DateTime StartDate { get; init; }
+  public required DateTime EndDate { get; init; }
+}
 
 public partial class Program { }
