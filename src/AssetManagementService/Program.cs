7512a720120c52ddba773fbc7c7ec8c601aadb71
zabronax <zabronax@gmail.com>
2025-12-04 10:46:06 +0100

    green: update api to fulfill asset requisition specification

--- src/AssetManagementService/Program.cs
+++ src/AssetManagementService/Program.cs
@@ -3,9 +3,26 @@ using Contracts.AssetManagement;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var assets = new Dictionary<string, Asset>();
+
 app.MapGet("/health", () => "Ok");
-app.MapPost("/assets", (AssetRegistrationRequest request) => new { Id = Guid.NewGuid() });
-app.MapGet("/assets/{id}", (string id) => new Asset { Id = id, Name = "Test Asset" });
+
+app.MapPost("/assets", (AssetRegistrationRequest request) =>
+{
+  var asset = new Asset { Id = Guid.NewGuid().ToString(), Name = request.Name };
+  assets.Add(asset.Id, asset);
+  var response = new AssetRegistrationResponse { Id = asset.Id };
+  return Results.Ok(response);
+});
+
+app.MapGet("/assets/{id}", (string id) =>
+{
+  if (assets.TryGetValue(id, out var asset))
+  {
+    return Results.Ok(asset);
+  }
+  return Results.NotFound();
+});
 
 app.Run();
 
