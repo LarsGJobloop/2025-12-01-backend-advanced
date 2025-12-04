07d36d64f5bfe4512ea394ecb0968d3ded99b0ab
zabronax <zabronax@gmail.com>
2025-12-04 09:50:35 +0100

    green: implement invalid registration request specification

--- src/AssetManagementService/Program.cs
+++ src/AssetManagementService/Program.cs
@@ -2,9 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/health", () => "Ok");
-app.MapPost("/assets", () => new { Id = Guid.NewGuid() });
+app.MapPost("/assets", (AssetRegistrationRequest request) => new { Id = Guid.NewGuid() });
 
 app.Run();
+
+class AssetRegistrationRequest
+{
+  public required string Name { get; set; }
+}
 
 // Needed for the MVC test framework
 public partial class Program { }
