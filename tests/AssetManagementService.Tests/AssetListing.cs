cceb343e3df79330554fbb6ca0c9c29aa84b7d57
zabronax <zabronax@gmail.com>
2025-12-04 13:54:47 +0100

    red: add existing asset listing specification

--- tests/AssetManagementService.Tests/AssetListing.cs
+++ tests/AssetManagementService.Tests/AssetListing.cs
@@ -31,4 +31,26 @@ public class AssetListing : IClassFixture<WebApplicationFactory<Program>>
     Assert.NotNull(assets);
     Assert.Empty(assets);
   }
+
+  [Fact]
+  public async Task GivenAnExistingAsset_WhenIListTheAssets_TheResponseIsAListWithTheAsset()
+  {
+    // Given a registered asset
+    var client = _factory.CreateClient();
+    var registration = new AssetRegistrationRequest { Name = "Test Asset" };
+    var response = await client.PostAsJsonAsync("/assets", registration);
+    var existingAsset = await response.Content.ReadFromJsonAsync<AssetRegistrationResponse>();
+    Assert.NotNull(existingAsset);
+
+    // When I list the assets
+    var listResponse = await client.GetAsync("/assets");
+    listResponse.EnsureSuccessStatusCode();
+
+    // Then the response is a list with the asset
+    var assets = await listResponse.Content.ReadFromJsonAsync<List<Asset>>();
+    Assert.NotNull(assets);
+    Assert.Single(assets);
+    Assert.Equal(existingAsset.Id, assets[0].Id);
+    Assert.Equal(registration.Name, assets[0].Name);
+  }
 }
