1417713f211060609b98906d537c0ed741179a72
zabronax <zabronax@gmail.com>
2025-12-04 10:39:01 +0100

    green: implement asset requisition spefication

--- src/AssetManagementService/Program.cs
+++ src/AssetManagementService/Program.cs
@@ -5,6 +5,7 @@ var app = builder.Build();
 
 app.MapGet("/health", () => "Ok");
 app.MapPost("/assets", (AssetRegistrationRequest request) => new { Id = Guid.NewGuid() });
+app.MapGet("/assets/{id}", (string id) => new Asset { Id = id, Name = "Test Asset" });
 
 app.Run();
 
