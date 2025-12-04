803f852bebdac7436935f15a87ae7cbf4a6f418e
zabronax <zabronax@gmail.com>
2025-12-04 16:27:37 +0100

    green: implement double booking specification

--- src/ReservationService/Program.cs
+++ src/ReservationService/Program.cs
@@ -3,17 +3,28 @@ using Contracts.ReservationService;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var reservations = new Dictionary<string, ReservationResponse>();
+
 app.MapGet("/health", () => "OK");
 
 app.MapPost("/reservations", (ReservationRequest request) =>
 {
-  return Results.Ok(new ReservationResponse
+  if (reservations.ContainsKey(request.AssetId))
+  {
+    return Results.BadRequest();
+  }
+
+  var newReservation = new ReservationResponse
   {
     Id = Guid.NewGuid(),
     AssetId = request.AssetId,
     StartDate = request.StartDate,
     EndDate = request.EndDate
-  });
+  };
+
+  reservations[request.AssetId] = newReservation;
+
+  return Results.Ok(newReservation);
 });
 
 app.Run();
