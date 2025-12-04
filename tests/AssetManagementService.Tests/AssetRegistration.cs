f1e4b77b986e2fb0ce426f369c2653acb0d1ec59
zabronax <zabronax@gmail.com>
2025-12-04 10:31:13 +0100

    red: add existing asset request spec

--- tests/AssetManagementService.Tests/AssetRegistration.cs
+++ tests/AssetManagementService.Tests/AssetRegistration.cs
@@ -48,4 +48,22 @@ public class AssetRegistration : IClassFixture<WebApplicationFactory<Program>>
     // Then the request is rejected
     Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
   }
+
+  [Fact]
+  public async Task GivenAnExistingAsset_WhenIRequestTheAsset_TheAssetIsReturned()
+  {
+    // Given a registered asset
+    var client = _factory.CreateClient();
+    var registration = new AssetRegistrationRequest { Name = "Test Asset" };
+    var response = await client.PostAsJsonAsync("/assets", registration);
+    var asset = await response.Content.ReadFromJsonAsync<AssetRegistrationResponse>();
+    Assert.NotNull(asset);
+    var assetId = asset.Id;
+
+    // When I request the asset by ID
+    var assetResponse = await client.GetFromJsonAsync<AssetRegistrationResponse>($"/assets/{assetId}");
+    Assert.NotNull(assetResponse);
+    Assert.Equal(assetId, assetResponse.Id);
+    Assert.Equal(registration.Name, assetResponse.Name);
+  }
 }
